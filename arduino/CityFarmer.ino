6f18aa8b031b1ef517edfc127b79ced2dcaa1f00
cheolbong <100348905+cheolbong@users.noreply.github.com>
2022-05-14 19:59:55 +0900

    동작은 10분마다 데이터는 1시간마다

--- arduino/CityFarmer.ino
+++ arduino/CityFarmer.ino
@@ -3,9 +3,9 @@
  * 1. 와이파이 연결
  * 2. 온도, 습도, 토양 수분, 조도 측정
  * 3. 데이터베이스에서 어플로 지정한 온도, 습도, 토양 수분, 조도 값 받아오기
- * 4. 데이터베이스에 전송
+ * 4. 데이터베이스에 전송(처음 값 보내고 1시간 뒤 다시 보냄)
  * 5. if문을 활용하여 온도, 습도, 토양 수분, 조도 조정 
- * 6. 아두이노 딥슬립모드 돌입(30분)
+ * 6. 아두이노 딥슬립모드 돌입(10분)
  */
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
@@ -19,7 +19,7 @@
 #define pen_power D5
 #define led_power D6
 #define moisture_power D7
-#define host "172.20.10.3"
+#define host "192.168.4.59"
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -36,6 +36,7 @@ int temp_goal_min = 0;
 int temp_goal_max = 0;
 int moisture_goal = 0;
 int moisture_goal_min = 0;
+int count = 6;
 
 void setup() {
   Serial.begin(115200);
@@ -51,6 +52,7 @@ void setup() {
 }
 
 void loop() {
+  count++;
   //dht 작동
   temp_value_read();
   Serial.print("습도 : ");
@@ -73,39 +75,43 @@ void loop() {
   //토양 수분 센서 끝
   
   //데이터베이스에 데이터 보내기 시작
-  WiFiClient client;
-  HTTPClient http;
+  if(count >= 6) {
+    WiFiClient client;
+    HTTPClient http;
 
-  if((WiFi.status() == WL_CONNECTED)) {
-    Serial.print("[HTTP] 서버 연결을 시도합니다...");
-    http.begin(client, "http://" host "/insert.php");
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    if((WiFi.status() == WL_CONNECTED)) {
+      Serial.print("[HTTP] 서버 연결을 시도합니다...");
+      http.begin(client, "http://" host "/insert.php");
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
 
-    Serial.print("[HTTP] 수집한 값의 POST 요청을 시도합니다...");
-    String POSTBODY = String("case=case1");
-    POSTBODY.concat(String("&temp="));
-    POSTBODY.concat(t);
-    POSTBODY.concat(String("&moisture="));
-    POSTBODY.concat(h);
-    POSTBODY.concat(String("&illuminance="));
-    POSTBODY.concat(cdsValue);
-    POSTBODY.concat(String("&water="));
-    POSTBODY.concat(moisture_percentage);
-    int httpCode = http.POST(POSTBODY);
+      Serial.print("[HTTP] 수집한 값의 POST 요청을 시도합니다...");
+      String POSTBODY = String("case=case1");
+      POSTBODY.concat(String("&temp="));
+      POSTBODY.concat(t);
+      POSTBODY.concat(String("&moisture="));
+      POSTBODY.concat(h);
+      POSTBODY.concat(String("&illuminance="));
+      POSTBODY.concat(cdsValue);
+      POSTBODY.concat(String("&water="));
+      POSTBODY.concat(moisture_percentage);
+      int httpCode = http.POST(POSTBODY);
 
-    if(httpCode > 0) {
-      Serial.printf("[HTTP] 응답 Code : %d\n", httpCode);
-      if(httpCode == HTTP_CODE_OK) {
-        const String& payload = http.getString();
-        Serial.print("서버로부터 수신된 응답 : ");
-        Serial.print(payload);
-        Serial.print("");
+      if(httpCode > 0) {
+        Serial.printf("[HTTP] 응답 Code : %d\n", httpCode);
+        if(httpCode == HTTP_CODE_OK) {
+          const String& payload = http.getString();
+          Serial.print("서버로부터 수신된 응답 : ");
+          Serial.print(payload);
+          Serial.print("");
+          count = 0;
+        }
+      } else {
+        Serial.printf("[HTTP] POST 요청이 실패했습니다. 오류 : %s\n", http.errorToString(httpCode).c_str());
       }
-    } else {
-      Serial.printf("[HTTP] POST 요청이 실패했습니다. 오류 : %s\n", http.errorToString(httpCode).c_str());
-    }
     http.end();
+    }
   }
+  
   //데이터베이스 전송 끝
   /*
    * 온도, 습도, 조도, 토양 수분 값 받아오기
@@ -135,7 +141,7 @@ void loop() {
       digitalWrite(led_power, LOW);
     }
   }
-  ESP.deepSleep(3600e6);
+  ESP.deepSleep(600e6);
 }
 
 void temp_value_read(){
